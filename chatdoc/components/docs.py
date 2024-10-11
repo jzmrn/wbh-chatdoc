0695aae9a913c06dab41199173bacefb3314d765
Jan Zimmermann <jzmrn@gmx.de>
2024-10-11 19:46:48 +0200

    feat: upload files to a tmp dir

--- chatdoc/components/docs.py
+++ chatdoc/components/docs.py
@@ -1,17 +1,29 @@
+import os
+
 import reflex as rx
 
 from .common import content, header
 from .navbar import navbar
 
+UPLOAD_ID = "upload"
 
-class UploadExample(rx.State):
+
+class UploadHandler(rx.State):
     uploading: bool = False
     progress: int = 0
-    total_bytes: int = 0
 
     async def handle_upload(self, files: list[rx.UploadFile]):
+        self.progress = 0
+
+        # TODO: avoid collisions and delete temporary files
+        folder = "tmp"
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
         for file in files:
-            self.total_bytes += len(await file.read())
+            print(f"Processing {file.filename}")
+            with open(f"./{folder}/{file.filename}", "wb") as f:
+                f.write(await file.read())
 
     def handle_upload_progress(self, progress: dict):
         self.uploading = True
@@ -21,38 +33,35 @@ class UploadExample(rx.State):
 
     def cancel_upload(self):
         self.uploading = False
-        return rx.cancel_upload("upload3")
+        return rx.cancel_upload(UPLOAD_ID)
 
 
 def upload_form():
     return rx.vstack(
         rx.upload(
             rx.text("Drag and drop files here or click to select files"),
-            id="upload3",
             border="1px dotted rgb(107,99,246)",
+            id=UPLOAD_ID,
             padding="5em",
+            accept={
+                "application/pdf": [".pdf"],
+            },
+            max_files=5,
         ),
-        rx.vstack(rx.foreach(rx.selected_files("upload3"), rx.text)),
-        rx.progress(value=UploadExample.progress, max=100),
-        rx.cond(
-            ~UploadExample.uploading,
-            rx.button(
-                "Upload",
-                on_click=UploadExample.handle_upload(
-                    rx.upload_files(
-                        upload_id="upload3",
-                        on_upload_progress=UploadExample.handle_upload_progress,
-                    ),
+        rx.divider(),
+        rx.heading("Selected files:"),
+        rx.vstack(rx.foreach(rx.selected_files(UPLOAD_ID), rx.text)),
+        rx.divider(),
+        rx.progress(value=UploadHandler.progress, max=100),
+        rx.button(
+            "Upload",
+            on_click=UploadHandler.handle_upload(
+                rx.upload_files(
+                    upload_id=UPLOAD_ID,
+                    on_upload_progress=UploadHandler.handle_upload_progress,
                 ),
             ),
-            rx.button(
-                "Cancel",
-                on_click=UploadExample.cancel_upload,
-            ),
-        ),
-        rx.text(
-            "Total bytes uploaded: ",
-            UploadExample.total_bytes,
+            disabled=UploadHandler.uploading,
         ),
         align_self="center",
     )
