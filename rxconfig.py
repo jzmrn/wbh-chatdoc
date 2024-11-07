a30f9794f2c23ed2079f1514a09a63be9b6ee533
Jan Zimmermann <jzmrn@gmx.de>
2024-11-07 21:38:59 +0100

    fix: release frontend with right url

--- rxconfig.py
+++ rxconfig.py
@@ -1,5 +1,5 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="chatdoc", api_url="https://chatdoc.dev-data.argus.intranet.basf.com"
+    app_name="chatdoc", api_url="https://chatdoc.data.argus.intranet.basf.com"
 )
